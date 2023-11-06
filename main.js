5bbe57b0322c4be2ff87de7861f4578031464a2b
Thieen226 <t.ho1@my.metroed.net>
2023-11-06 09:48:34 -0800

    add

--- main.js
+++ main.js
@@ -59,10 +59,11 @@ const item ={
 }
 
 //create variables for both the Add to Cart button and the itemContainer
-const addBtn = document.getElementById("addBtn");
-const itemContainer = document.getElementById("itemContainer");
-let totalValue = 0;
+let addBtn = document.getElementById("addBtn");
+let itemContainer = document.getElementById("itemContainer");
 let total = document.getElementById("total");
+let totalValue = 0;
+let checkoutBtn = document.getElementById("checkoutBtn");
 
 function addItem(){
     //create variables that hold the barcode of the item and its quantity
@@ -77,13 +78,16 @@ function addItem(){
     else if(itemQuantity === ""){
         alert("You have to add numbers of item you want");
     }
-    //if the barcode of the item is inside of item objects then it will be added to the cart
+    //if the barcode of the item is inside of item objects then it will be added to the cart section
     else if(item.hasOwnProperty(itemBarcode)){
+
+        //creating elements to store the iten information
         const container = document.createElement("div");
         const pItem = document.createElement("p");
         const pPrice = document.createElement("p");
         const pQuantity = document.createElement("p");
 
+        //adding the new elements into existed elements
         itemContainer.appendChild(container);
         container.appendChild(pItem);
         container.appendChild(pPrice);
@@ -103,6 +107,9 @@ function addItem(){
         //change the total according to the amount of items and their prices
         totalValue += parseFloat(itemQuantity * item[itemBarcode].price);
         total.innerText= "Total: $" + totalValue.toFixed(2);
+
+        //change the total grand (adding tax to the total)
+        grandTotal.innerText = " Your grand total (including tax, 9.25%) is $ " + (totalValue + totalValue*0.0925).toFixed(2); 
     }
     //if the barcode of the item is not in the item object, alert this
     else{
@@ -112,7 +119,9 @@ function addItem(){
     //reset the input of itemBarcode and quantity
     document.getElementById("itemBarcode").value = "";
     document.getElementById("quantity").value = "";
-
-
 }
+
+//after clicking the Add to Cart button, the item you scanned will appear below the cart section
 addBtn.addEventListener("click", addItem);
+
+
