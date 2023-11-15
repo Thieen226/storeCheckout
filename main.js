bdcfb4be9b4d46111b476ede5f99932edd5501b8
Thien Ho <t.ho1@my.metroed.net>
2023-11-14 22:36:10 -0800

    add

--- main.js
+++ main.js
@@ -86,7 +86,7 @@ function addItem(){
         
         //checking if the item is already exist, if it is then update the quantity
         if(existItem.hasOwnProperty(itemBarcode)){
-            
+
         }
         else{
         //creating elements to store the item information
@@ -116,6 +116,7 @@ function addItem(){
         //updating the quantity and apply it to the total 
         existItem[itemBarcode] = itemQuantity;
 
+        
         //change the total according to the amount of items and their prices
         totalValue += parseFloat(itemQuantity * item[itemBarcode].price);
         total.innerText= "Total: $" + totalValue.toFixed(2);
@@ -127,12 +128,9 @@ function addItem(){
     else{
         alert("Item is not available");
     }
-
     //reset the input of itemBarcode and quantity
     document.getElementById("itemBarcode").value = "";
     document.getElementById("itemQuantity").value = "";
-
-
 }
 
 //after clicking the Add to Cart button, the item you scanned will appear below the cart section
